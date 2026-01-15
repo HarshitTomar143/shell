eb916f69f3b1c6840824a7eec8ddbb748b584229
Harshittomar <tomarharshit2023@gmail.com>
2026-01-15 20:38:54 +0530

    fixed rust-analyser

---
+++ target/debug/deps/rshell-5b4a351437e3ccdd.d
@@ -0,0 +1,5 @@
+F:\Go\Project\rshell\target\debug\deps\librshell-5b4a351437e3ccdd.rmeta: src\main.rs
+
+F:\Go\Project\rshell\target\debug\deps\rshell-5b4a351437e3ccdd.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/rshell-fadeb483ea9b98c1.d
@@ -0,0 +1,5 @@
+F:\Go\Project\rshell\target\debug\deps\librshell-fadeb483ea9b98c1.rmeta: src\main.rs
+
+F:\Go\Project\rshell\target\debug\deps\rshell-fadeb483ea9b98c1.d: src\main.rs
+
+src\main.rs:
